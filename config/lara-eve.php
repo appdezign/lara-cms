a735d2cb2e53cfaf9c9c9904025a352bd72095f2
Sybrand Hoeksma <s.hoeksma@firmaq.nl>
2024-09-16 16:00:54 +0200

    Add override to Front list

--- config/lara-eve.php
+++ config/lara-eve.php
@@ -6,4 +6,15 @@ return [
 		'service' => []
 	],
 
+	'override_front_entity_objects' => [
+		'event' => [
+			'sortfield' => 'startdate',
+			'sortorder' => 'asc',
+			'sortfield2nd' => 'starttime',
+			'sortorder2nd' => 'asc',
+			// 'paginate' => 10,
+			// 'limit' => 25,
+		]
+	],
+
 ];
